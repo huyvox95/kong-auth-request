e53721c950c369e49e63aba96b3700c4e388104a
HuyVT19 <huyvt19@fpt.com.vn>
2022-09-12 18:25:30 +0700

    update

--- src/schema.lua
+++ src/schema.lua
@@ -1,10 +1,19 @@
-return {
-    no_consumer = true,
-    fields = {
+local typedefs = require "kong.db.schema.typedefs"
+
+local PLUGIN_NAME = "kong-auth-request"
+
+local schema = {
+  name = PLUGIN_NAME,
+  fields = {
+    { consumer = typedefs.no_consumer },
+    { config = {
         timeout = { default = 10000, type = "number" },
         keepalive_timeout = { default = 60000, type = "number" },
         auth_uri = { required = true, type = "string" },
         origin_request_headers_to_forward_to_auth = { type = "array", default = {} },
         auth_response_headers_to_forward = { type = "array", default = {} }
     }
-}
+  }
+  },
+
+return schema
